ef41de17dc32b001c0fc38c052be4c3e5a189e0f
Aryan Raj <166948766+Aaru2808@users.noreply.github.com>
2025-07-25 20:23:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (74.10%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
     string reverseWords(string s) {
+        //1.reverse the whole string
+        reverse(s.begin(),s.end());
+        int i=0;
+        int l =0,r=0;
+
+        //2.i,l,r le lege uske baad ham i ko agee badhate jayege jab tk space nhi aa jata hai or r ko assign krte jayege
+        int n=s.size();
+        while(i<n){
+            while(i<n&& s[i]!=' '){
+                s[r++]=s[i++];
+
+            }
+            if(l<r){
+                reverse(s.begin()+l,s.begin()+r);
+                s[r]=' ';
+                r++;
+                l=r;
+
+            }
+            i++;
+        }
+        //3.jab space aa jayega tb hm l ot r tk phir se resver kr dege particular word
+        //4.last me ham substr ke help se space ko hta dege jo ki last me hoga
+        s= s.substr(0,r-1);
+        return s;
+
+
         
-        stringstream ss(s);
-        string token;
-        string result="";
-        int n= result.length();
-        while(ss>>token){
-            result=token+" "+result;
-        }
-        if(!result.empty() && result.back()==' '){
-             result.pop_back();
-
-        }
-        return result;
-
-      
-    } 
-    
+    }
 };
